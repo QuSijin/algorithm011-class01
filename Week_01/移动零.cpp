5f320fc7065e3f1abcef60b4483685a2bdcd8f25
QuSijin <775607737@qq.com>
2020-06-27 16:53:00 +0800

    简单题型之移动零

---
+++ Week_01/移动零.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[j],nums[i]);
+                // int temp=nums[j];
+                // nums[j]=nums[i];
+                // nums[i]=temp;
+                j++;
+            }
+            
+        }
+    }
+};
