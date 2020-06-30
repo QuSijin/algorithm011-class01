1b7d02920b4eaf08511d0805e6e75a058ced8aec
QuSijin <775607737@qq.com>
2020-06-30 20:28:48 +0800

    note

--- Week_02/vaild-anagram.cpp
+++ Week_02/vaild-anagram.cpp
@@ -6,7 +6,7 @@ public:
      vector<int> counter(26,0);
     for (int i = 0; i < s.size(); i++) {
         counter[s.at(i) - 'a']++;
-        counter[t.at(i) - 'a']--;
+        counter[t.at(i) - 'a']--;//s 负责在对应位置增加，t 负责在对应位置减少
     }
     for (int count : counter) {
         if (count != 0) {
