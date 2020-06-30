f16a8eaa9ada7c8c0319408b5da0fa8564517897
QuSijin <775607737@qq.com>
2020-06-30 21:31:54 +0800

    两数之和的hash解法

--- Week_01/两数之和.cpp
+++ Week_01/两数之和.cpp
@@ -1,3 +1,4 @@
+1.暴力法--简单易懂
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -14,5 +15,25 @@ public:
             }
         }
         return a;
+    }
+};
+2.一次hash--O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         unordered_map<int,int> ma;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (ma.find(complement)!= ma.end()) {
+		//find函数主要实现的是在容器内查找指定的元素
+		//查找成功返回一个指向指定元素的迭代器，查找失败返回end迭代器。
+		//因此此处!=end()意为查找成功
+                return { ma[complement], i };
+		//m[complement]为已经加入map的元素的索引，所以小于本轮循环中的i，放在前面
+            }
+            ma[nums[i]]= i;//向map中添加元素
+        }
+       return {};
+
     }
 };
