2ce9835bb6f7bae20d667daf040ff4cdba104ec5
QuSijin <775607737@qq.com>
2020-06-30 16:09:46 +0800

    哈希表解法--有效的字母异位词

---
+++ Week_02/vaild-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+   if(s.size()!=t.size())
+        return false;
+     vector<int> counter(26,0);
+    for (int i = 0; i < s.size(); i++) {
+        counter[s.at(i) - 'a']++;
+        counter[t.at(i) - 'a']--;
+    }
+    for (int count : counter) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
+
+    }
+};
