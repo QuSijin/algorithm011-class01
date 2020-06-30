c2b80d04348f5cf05804d6f939b9f9c117ed9615
QuSijin <775607737@qq.com>
2020-06-30 21:38:02 +0800

    更改注释

--- Week_02/two-sum.cpp
+++ Week_02/two-sum.cpp
@@ -10,7 +10,7 @@ public:
                 //查找成功返回一个指向指定元素的迭代器，查找失败返回end迭代器。
                 //因此此处!=end()意为查找成功
                 return { ma[complement], i };
-                //m[complement]为已经加入map的元素的索引，所以小于本轮循环中的i>，放在前面
+                //ma[complement]为已经加入map的元素的索引，所以小于本轮循环中的i>，放在前面
             }
             ma[nums[i]]= i;//向map中添加元素
         }
