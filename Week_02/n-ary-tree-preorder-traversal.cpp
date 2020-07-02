a9b6cc1460220d895f72c2fad0e5d29053371af6
QuSijin <775607737@qq.com>
2020-07-02 09:52:53 +0800

    N叉树的前序遍历--根左右

--- Week_02/n-ary-tree-preorder-traversal.cpp
+++ Week_02/n-ary-tree-preorder-traversal.cpp
@@ -12,4 +12,21 @@ public:
     }
 };
 2.迭代
-
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        if(!root)   return res;
+        stack<Node*> stk;
+        stk.push(root);
+        while(!stk.empty()){
+            Node* top = stk.top(); 
+            res.push_back(top -> val);//u 
+            stk.pop();//1.u出 2.v1、v2、v3出
+            for(int i = top -> children.size()-1; i >= 0; --i){
+                stk.push(top -> children[i]);//v3、v2、v1
+            }
+        }
+        return res;//u、v1、v2、v3
+    }
+};
