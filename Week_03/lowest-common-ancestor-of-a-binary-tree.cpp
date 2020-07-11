87b9fb02813b359f050c6b544304ac5e56faf667
QuSijin <50854759+QuSijin@users.noreply.github.com>
2020-07-11 09:49:10 +0800

    Update lowest-common-ancestor-of-a-binary-tree.cpp
    note

--- Week_03/lowest-common-ancestor-of-a-binary-tree.cpp
+++ Week_03/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,7 +8,8 @@ public:
         if ((lson && rson) || ((root->val == p->val || root->val == q->val) && (lson || rson))) {
             ans = root;
         } 
-        
+        //(lson && rson)-->左子树和右子树均包含 p节点或 q节点，如果左子树包含的是 p 节点，那么右子树只能包含 q 节点
+        //((root->val == p->val || root->val == q->val) && (lson || rson)))-->x 恰好是 p节点或 q节点且它的左子树或右子树有一个包含了另一个节点
         return lson || rson || (root->val == p->val || root->val == q->val);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -16,4 +17,3 @@ public:
         return ans;
     }
 };
-
