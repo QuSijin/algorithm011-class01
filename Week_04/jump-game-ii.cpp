05139c0adcca4623be2b848ad7c8a7235e5452bb
QuSijin <775607737@qq.com>
2020-07-16 15:37:55 +0800

    增加了是否出界的查看，减少了不必要的循环

--- Week_04/jump-game-ii.cpp
+++ Week_04/jump-game-ii.cpp
@@ -3,13 +3,13 @@ public:
     int jump(vector<int>& nums) {
         int maxPos = 0, n = nums.size(), end = 0, step = 0;
         for (int i = 0; i < n - 1; ++i) {//因为是起跳的时候就 + 1 了，如果最后一次跳跃刚好到达了最后一个位置，那么遍历到最后一个位置的时候就会再次起跳，这是不允许的，因此不能遍历最后一个位置
-            if (maxPos >= i) {
                 maxPos = max(maxPos, i + nums[i]);
+		if(maxPos >= nums.size()-1) return (step+1);
                 if (i == end) {
                     end = maxPos;
                     ++step;
                 }
-            }
+            
         }
         return step;
     }
