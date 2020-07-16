034676f1e4609f694703454d06cfb91b1f5cc599
QuSijin <775607737@qq.com>
2020-07-16 09:12:57 +0800

    柠檬水找零--贪心

---
+++ Week_04/lemonade-change.cpp
@@ -0,0 +1,28 @@
+//贪心体现在优先找10+5而不是5+5+5
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for (int bill: bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
